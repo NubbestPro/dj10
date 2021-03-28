f95b1b7c30f55fdb8690f346d00c49f340ec6665
LAPTOP-LBM6I7BH\gavin <gavinchen1003@gmail.com>
2021-03-27 20:51:34 -0400

    dj10

---
+++ portfolio/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+#add blog app into setting, url, views, html
+#title CharField
+#pub_date DateTimeField
+#body TextField
+#image ImageField
+
+#python manage.py makemigrations
+#python manage.py migrate
+#add to admin
+#def __str
+
+#update views
+#update blog.html
